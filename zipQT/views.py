98d0587e877e5d3dc2ca290945979704ebd59f26
Joboa <joboa2015@gmail.com>
2020-08-12 13:04:53 +0000

    added gitignore file and requirements.txt

--- zipQT/views.py
+++ zipQT/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import redirect
 from django.core.files.storage import FileSystemStorage
 from django.contrib import messages
 from django.core.management.base import BaseCommand, CommandError
+from django.core.exceptions import ObjectDoesNotExist
+
+from .validators import validate_file_extension
 import uuid
 import os
 
@@ -22,21 +25,23 @@ def help(request):
 def convert(request):
     if request.method == 'POST':
         uploaded_file = request.FILES['document']
+        #fs = FileSystemStorage(validators = [validate_file_extension(uploaded_file)])
         name = str(uuid.uuid4())
         fs.save(name+".txt", uploaded_file)
-        file_output = os.system('text2qti ' + os.path.join(MEDIA_ROOT, name+".txt"))
-        print(file_output)
-        if file_output is DoesNotExist:
-            redirect('zipQT-home')
-        
+        os.system('text2qti ' + os.path.join(MEDIA_ROOT, name+".txt"))
+      
         with open(os.path.join(MEDIA_ROOT, name+".zip"), 'rb') as fh:
             response = HttpResponse(fh.read(), content_type="application/zip")
             response['Content-Disposition'] = 'inline; filename=' + uploaded_file.name + ".zip"
-            response['Location'] = 'zipQT/success_download.html'
-            fs.delete(uploaded_file)
             return response
         raise Http404
+
+    else:
+        redirect('zipQT-home')
+
     return render(request, 'zipQT/convert.html')
+
+
 
 
 
